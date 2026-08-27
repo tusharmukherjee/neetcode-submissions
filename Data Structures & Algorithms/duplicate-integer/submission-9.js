208d110ea1c39da19c8c2a21a3d75ca60d5ed0c5
Tushar Mukherjee <74879193+tusharmukherjee@users.noreply.github.com>
2026-08-27 09:36:58 +0530

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        // let arr = [];
+        // for(let i = 0; i<=nums.length-1; i++){
+        //     if(arr[nums[i]] == nums[i]) return true;
+        //     arr[nums[i]] = nums[i]
+        // }
+        // return false
+        let arr = new Set(nums);
+        if(arr.size != nums.length) return true
+        return false
+    }
+}
