b32d9f4dcc966f478d72c39be73c7eb5a57c4591
Tushar Mukherjee <74879193+tusharmukherjee@users.noreply.github.com>
2026-09-04 00:13:04 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const hashMap = new Map();
+        const groupArr = [];
+
+        for (let i = 0; i < strs.length; i++) {
+            const hash = new Array(26).fill(0);
+
+            for (const char of strs[i]) {
+                const hashIndex = char.charCodeAt(0) - 97;
+                hash[hashIndex]++;
+            }
+
+            const key = hash.join("#");
+
+            const alreadyPresent = hashMap.get(key) || [];
+            hashMap.set(key, [...alreadyPresent, strs[i]]);
+        }
+
+        for (const [, eachArr] of hashMap.entries()) {
+            groupArr.push(eachArr);
+        }
+
+        return groupArr;
+    }
+}
