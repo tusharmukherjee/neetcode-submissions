bb8bbe3192f20f69081d52e31c7d5b18dff0dcdb
Tushar Mukherjee <74879193+tusharmukherjee@users.noreply.github.com>
2026-09-08 13:29:27 +0530

    Add: remove-duplicates-from-sorted-array - submission-9

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-9.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let left = 0;
+        let right = 1;
+        let k = 1;
+        if(nums.length === 0) return 0
+        while(right<nums.length){
+            if(nums[left] != nums[right]){
+                nums[k] = nums[right]
+                left = right;
+                k++;
+            }
+            right++
+        }
+        return k;
+    }
+}
